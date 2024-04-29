1a6115c73c7635e22cc95e419742c61dde1d51a4
tuojing <a24364042@gmail.com>
2024-04-29 21:41:49 +0800

    initial commit

--- cmdTest.py
+++ cmdTest.py
@@ -1,9 +1,9 @@
 import argparse
 
-parser = argparse.ArgumentParser()  # 建立解析命令的物件
-parser.add_argument('src', help='來源資料夾路徑')
-parser.add_argument('dest', help='目標資料夾路徑')
+parser = argparse.ArgumentParser()
+parser.add_argument('src', help='來源檔案路徑')
+parser.add_argument('dest', help='目標檔案路徑')
 
-args = parser.parse_args()  # 解析命令參數
+args = parser.parse_args()
 print('來源路徑:', args.src)
 print('目標路徑:', args.dest)
